e81add62ba602a85d5a7437ff301222d4561d576
shineeshkp <49532585+shineeshkp@users.noreply.github.com>
2020-06-13 15:59:35 +0530

    Add files via upload

--- glass.py
+++ glass.py
@@ -22,7 +22,6 @@ import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import seaborn as sns
 get_ipython().run_line_magic('matplotlib', 'inline')
 import pickle
 
